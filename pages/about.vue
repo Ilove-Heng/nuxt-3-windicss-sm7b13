d9b584df48e95d407b9a78e63b053b6a264f44a4
Ilove-Heng <102523138+Ilove-Heng@users.noreply.github.com>
2023-09-06 14:51:08 +0700

    2 files modified, 2 added

---
+++ pages/about.vue
@@ -0,0 +1,4 @@
+<template>
+  <h1>about page</h1>
+  <button><NuxtLink >Back home</NuxtLink></li>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <main>
+  <header>
+    <nav>
+      <ul>
+        <li><NuxtLink to="/about">About</NuxtLink></li>
+        <li><NuxtLink to="/posts/1">Post 1</NuxtLink></li>
+        <li><NuxtLink to="/posts/2">Post 2</NuxtLink></li>
+      </ul>
+    </nav>
+  </header>
+
+    <listItem
+      v-for="(item, index) in database"
+      :name="item.name"
+      :department="item.deparment"
+      :salary="item.salary"
+      :position="index"
+      v-on:deleteItem="deleteItem"
+    />
+  </main>
+</template>
+
+<script setup>
+let database = ref([
+  {
+    name: 'Johnny',
+    deparment: 'Marketing',
+    salary: 30000,
+  },
+  {
+    name: 'Sarah',
+    deparment: 'Engineering',
+    salary: 72000,
+  },
+  {
+    name: 'Terrance',
+    deparment: 'Executive',
+    salary: 125000,
+  },
+]);
+
+function deleteItem(position) {
+  database.value.splice(position, 1);
+}
+</script>
+
+<style>
+.some {
+  &-class {
+    @apply text-green-300;
+  }
+}
+</style>
