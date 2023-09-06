fe998fc1b5fe0bb69cc12a767e49337f4bb29618
Ilove-Heng <102523138+Ilove-Heng@users.noreply.github.com>
2023-09-06 14:34:31 +0700

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,7 @@
+import { defineNuxtConfig } from 'nuxt';
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  // builder: 'webpack',
+  modules: ['nuxt-windicss'],
+});
